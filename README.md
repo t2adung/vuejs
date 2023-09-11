738e6d37a4676f053978f8df3ccf15f7426b62cd
Tran Thi Ai Dung <dungtta@DungTTA.local>
2023-09-11 16:25:02 +0700

    Build a rock, paper, scissors 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vuejs
 Practice VueJS
+
+https://www.youtube.com/watch?v=Ls1WfzOrKkc&list=PLR8vUZDE6IeO_xCvdbfT1rtL4r4jc8ixm
+1. Learn VUE 3 by Making a Web App ~ Composition API, Vue JS Hooks? 
+https://www.youtube.com/watch?v=Ls1WfzOrKkc
---
+++ vue-rock-paper-scissors/src/App.vue
@@ -0,0 +1,113 @@
+<script setup>
+import  { ref, computed, onMounted } from 'vue'
+
+/* Trong Vue 3, ref là một hàm được sử dụng để tạo một biến tham chiếu (reference) có khả năng theo dõi và tự động cập nhật giao diện người dùng khi giá trị của biến này thay đổi. 
+Biến tham chiếu (ref) thường được sử dụng để lưu trữ các giá trị dữ liệu có khả năng thay đổi trong ứng dụng Vue.*/
+const wins = ref(0) // tạo một biến tham chiếu có tên là losses và khởi tạo nó với giá trị ban đầu là 0.
+const draws = ref(0)
+const losses = ref(0) 
+
+const choice = ref(null)
+const computerChoice = ref(null)
+const verdict = ref(null)
+
+const outcomes = {
+	'rock': {
+		'rock': 'draw',
+		'paper': 'loss',
+		'scissors': 'win'
+	},
+	'paper': {
+		'rock': 'win',
+		'paper': 'draw',
+		'scissors': 'loss'
+	},
+	'scissors': {
+		'rock': 'loss',
+		'paper': 'win',
+		'scissors': 'draw'
+	}
+}
+
+const play = (c) => {
+	choice.value = c
+	
+	const choices = ['rock', 'paper', 'scissors']
+	const random = Math.floor(Math.random() * choices.length)
+	computerChoice.value = choices[random]
+
+	const outcome = outcomes[choice.value][computerChoice.value]
+	
+	if (outcome === 'win') {
+		wins.value++
+		verdict.value = 'You win!'
+	} else if (outcome === 'loss') {
+		losses.value++
+		verdict.value = 'You Lose!'
+	} else {
+		draws.value++
+		verdict.value = 'It\'s a draw'
+	}
+
+	SaveGame()
+}
+
+const SaveGame = () => {
+	localStorage.setItem('wins', wins.value)
+	localStorage.setItem('draws', draws.value)
+	localStorage.setItem('losses', losses.value)
+}
+
+const LoadGame = () => {
+	wins.value = parseInt(localStorage.getItem('wins')) || 0
+	draws.value = parseInt(localStorage.getItem('draws')) || 0
+	losses.value = parseInt(localStorage.getItem('losses')) || 0
+}
+
+onMounted(() => {
+	LoadGame()
+})
+
+
+</script>
+
+<template>
+  <div v class="bg-gray-700 text-white text-center min-h-screen flex flex-col">
+    <header class="container mx-auto p-6">
+      <h1 class="text-4xl font-bold">Rock, Paper, Scissors!</h1>
+    </header>
+
+    <main class="container mx-auto p-6 flex-1">
+			<div v-if="choice === null" class="flex items-center justify-center -mx-6">
+        <button @click="play('rock')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-pink-500">
+					<img src="./assets/RockIcon.svg" alt="Rock" class="w-full" />
+				</button>
+
+				<button @click="play('paper')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-green-500">
+					<img src="./assets/PaperIcon.svg" alt="Paper" />
+				</button>
+
+				<button @click="play('scissors')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500">
+					<img src="./assets/ScissorsIcon.svg" alt="Scissors" />
+				</button>
+      </div>
+
+      <div v-else>
+        <div class="text-3xl mb-4">
+					You picked <span class="text-pink-500">{{ choice }}</span>
+				</div>
+				<div class="text-3xl mb-4">
+					The computer picked <span class="text-green-500">{{ computerChoice }}</span>
+				</div>
+				<div class="text-6xl mb-12">
+					{{ verdict }}
+				</div>
+
+      </div>
+		</main>
+  </div>
+</template>
+
