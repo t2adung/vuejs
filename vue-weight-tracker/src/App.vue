5c4a882efbb28396b0dfdba717cde6b52626d31d
t2adung <t2adung@gmail.com>
2023-09-13 15:29:12 +0700

    Build vue weight tracker 1

---
+++ vue-weight-tracker/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import  {ref, shallowRef, computed, watch, nextTick } from 'vue'
+
+const weights = ref([])
+
+const weightChartEl = ref(null)
+
+const weightChart = shallowRef(null)
+
+const weightInput = ref(0)
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+})
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date().getTime()
+  })
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>Weight Tracker</h1>
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>&nbsp;
+      <small>Current weight (kg)</small>
+    </div>
+
+    <form @submit.prevent="addWeight">
+      <input type="number" step="0.1" v-model="weightInput" />
+      <input type="submit" value="Add weight" />
+    </form>
+  </main>
+</template>
+
+<style>
+</style>
